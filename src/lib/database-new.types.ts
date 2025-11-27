f2055b1579b3b439201e35c1a983a54775b31ca2
Wickedawesome <Johnchuc@outlook.com>
2025-11-27 04:01:58 +0000

    fix: Remove corrupted database-new.types.ts file

--- src/lib/database-new.types.ts
+++
@@ -1,4 +0,0 @@
-Need to install the following packages:
-supabase@2.62.5
-Ok to proceed? (y) npm WARN deprecated node-domexception@1.0.0: Use your platform's native DOMException instead
-2025/11/27 03:14:08 Access token not provided. Supply an access token by running supabase login or setting the SUPABASE_ACCESS_TOKEN environment variable.
